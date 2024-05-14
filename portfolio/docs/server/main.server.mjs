5ad910660cd25de567e3fb2c35e71404a6d6cc43
aolavi <104347702+aolavi@users.noreply.github.com>
2024-05-14 17:16:16 +0200

    New build

--- portfolio/docs/server/main.server.mjs
+++ portfolio/docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-PJV7GMRA.mjs";import"./chunk-O222HON5.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-HPDIJAAH.mjs";import"./chunk-BIKI374M.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
